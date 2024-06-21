a2469529cf1ab6fe514b097f3d60803e3d33f4e2
christian garcia <christian.garcia@labbe.io>
2024-06-20 19:36:12 -0600

    ajustes

--- src/components/App.js
+++ src/components/App.js
@@ -1,86 +1,90 @@
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
-import PopupWithForm from './PopupWithForm';
-import React from 'react';
-import { api } from './Api';
-
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import PopupWithForm from "./PopupWithForm";
+import React from "react";
+import { api } from "./Api";
 
 function App() {
-
   const [openProfileOpen, setOpenProfileOpen] = React.useState(false);
-  const [openAddCardOpen, setOpenProfileOpen] = React.useState(false);
-  const [openAvatarOpen, setOpenProfileOpen] = React.useState(false);
+  const [openAddCardOpen, setOpenAddCardOpen] = React.useState(false);
+  const [openAvatarOpen, setOpenAvatarOpen] = React.useState(false);
   const [openImageOpen, setOpenImageOpen] = React.useState(false);
-
 
   const [currenUser, setCurrentUser] = React.useState(false);
   const [cards, setCards] = React.useState([]);
 
-
   const closeAllPopups = () => setOpenProfileOpen(false);
 
   React.useEffect(() => {
-    api.getUserInfo().then(user => {
+    api.getUserInfo().then((user) => {
       setCurrentUser(user);
-      api.getCards(). then(cards => {
-        setCards(cards)
-    })
-  })
-   },[])
+      api.getCards().then((cards) => {
+        setCards(cards);
+      });
+    });
+  }, []);
 
+  const handleCardClick = () => {}
+  const handleLike = () => {}
+  const handleDeleteCard = () => {}
+  const handleRemoveLike = () => {}
 
   return (
-      <div classNameName="page">
-        <Header
+    <div className="page">
+      <Header
         handleEditProfileClick={() => {
           setOpenProfileOpen(true);
         }}
         handleAddPlaceClick={() => {
           setOpenAddCardOpen(true);
         }}
-        />
-        <Main
+      />
+      <Main
         currenttUser={currenUser}
         cards={cards}
         handleCardClick={handleCardClick}
         handleLike={handleLike}
         handleRemoveLike={handleRemoveLike}
         handleDeleteCard={handleDeleteCard}
-        />
-        <Footer/>   
-        <PopupWithForm 
-        open= {openProfileOpen} onClose={closeAllPopups} title={'Edit profile'}>
-          <>
+      />
+      <Footer />
+      <PopupWithForm
+        open={openProfileOpen}
+        onClose={closeAllPopups}
+        title={"Edit profile"}
+      >
+        <>
           <input
-                placeholder="Name"
-                minLength="2"
-                maxLength="40"
-                type="text"
-                id="input-name"
-                className="form form_input"
-                name="name"
-                defaultValue="Jacques Cousteau"
-                required
-              />
-              <span className="form form_error form_error_index-name"></span>
-              <label for="input-name" className="popup__label popup__label_edit">
-              </label>
-              <input
-                placeholder="About"
-                required
-                minLength="2"
-                maxLength="200"
-                type="text"
-                id="input-about"
-                className="form form_input"
-                name="job"
-                defaultValue="Explorer"
-              />
-              <span className="form form_error form_error_index-job"></span>
-          </>
-          
-          </PopupWithForm> 
+            placeholder="Name"
+            minLength="2"
+            maxLength="40"
+            type="text"
+            id="input-name"
+            className="form form_input"
+            name="name"
+            defaultValue="Jacques Cousteau"
+            required
+          />
+          <span className="form form_error form_error_index-name"></span>
+          <label
+            htmlFor="input-name"
+            className="popup__label popup__label_edit"
+          ></label>
+          <input
+            placeholder="About"
+            required
+            minLength="2"
+            maxLength="200"
+            type="text"
+            id="input-about"
+            className="form form_input"
+            name="job"
+            defaultValue="Explorer"
+          />
+          <span className="form form_error form_error_index-job"></span>
+        </>
+      </PopupWithForm>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,56 +1,55 @@
-import ButtonTrash from '../images/Trash.svg'
+import ButtonTrash from "../images/Trash.svg";
 import React from "react";
 
-
-export default function Card ({
+export default function Card({
   title,
   link,
   handleCardClick,
   handleLike,
   handleRemoveLike,
   handleDeleteCard,
-  _id, 
-  likes, 
-  owner, 
+  _id,
+  likes,
+  owner,
   createdAt,
   user,
 }) {
-
   const hasOwnerLike = () => {
     return likes.some((item) => {
       return item._id === user._id;
     });
-  }
+  };
 
   const userIsOwner = () => {
     return owner._id === user._id;
-  }
+  };
 
   const onhandleLike = () => {
-    if(hasOwnerLike()){
-      handleRemoveLike(_id)
-    }else{
+    if (hasOwnerLike()) {
+      handleRemoveLike(_id);
+    } else {
       handleLike(_id);
     }
-  }
+  };
 
   return (
     <div className="element">
-          <img
-            src={link}
-            alt={title}
-            className="element__image"
-          />
-          {userIsOwner() && (
-            <button className="button-delete">
-            <img src= {ButtonTrash}</button>)}
-          <div className="element__place">
-            <h3 className="element__title">{title}</h3>
-            <button 
-             onClick={onhandleLike} className={`element__like ${hasOwnerLike() ? 'element__like-click': ''}`></button>
-            <p className="element__counter">{likes.length}</p>
-          </div>
-        </div>
-  )
+      <img src={link} alt={title} className="element__image" />
+      {userIsOwner() && (
+        <button className="button-delete">
+          <img src={ButtonTrash} alt="boton de basura"/>
+        </button>
+      )}
+      <div className="element__place">
+        <h3 className="element__title">{title}</h3>
+        <button
+          onClick={onhandleLike}
+          className={`element__like ${
+            hasOwnerLike() ? "element__like-click" : ""
+          }`}
+        ></button>
+        <p className="element__counter">{likes.length}</p>
+      </div>
+    </div>
+  );
 }
- 
