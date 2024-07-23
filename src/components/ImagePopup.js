deae26608c95c599429e82322a6fd481fc9ea37c
Cesar 26 <cesarauge26@gmail.com>
2024-07-23 16:00:37 -0500

    added ImagePopup.js

---
+++ src/components/ImagePopup.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Popup from "./Popup";
+
+
+export default function ImagePopup ({title, onClose, selectCard, open}){
+  
+
+  return (
+
+    <>
+    <Popup  title= {title} onClose={onClose} open={open}>
+      <div className="popup_content_image">
+      <h3 className="popup__title">{selectCard.title}</h3>
+      <img className="popup__image" src={selectCard.link} alt={selectCard.title}/>
+      </div>
+    </Popup>
+    </>
+  )
+
+}
