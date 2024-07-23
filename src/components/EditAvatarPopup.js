4a570c96c340b481a95b9eec2eb2e565ea4872c1
Cesar 26 <cesarauge26@gmail.com>
2024-07-22 22:02:13 -0500

    proyect 12 ready

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,37 +1,34 @@
 import PopupWithForm from "./PopupWithForm";
 import React from "react";
-import  CurrentUserContext  from "../contexts/CurrentUserContext";
+import CurrentUserContext from "../contexts/CurrentUserContext";
 
-export default function EditAvatarPopup({isOpen, onClose, onUpdateAvatar}) {
+export default function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
   const avatarRef = React.useRef();
 
-  function onSubmitAvatar(e) {
-    e.preventDefault();
-    onUpdateAvatar({
-      avatar: avatarRef.current.value,
-    });
-    avatarRef.current.value ="";
+  function onSubmitAvatar() {
+    onUpdateAvatar(
+      avatarRef.current.value,
+    );
+    avatarRef.current.value = "";
   }
   return (
-
-<PopupWithForm
-        open={isOpen}
-        onClose={onClose}
-        title={"Edit profile photo"}
-        onSubmit={onSubmitAvatar}
-
-      >
-        <>
-          <input
-            type="url"
-            required
-            className="form form_input"
-            name="link"
-            ref={avatarRef}
-            placeholder="link"
-          />
-          <span className="form form_error form_error_index-name"></span>
-        </>
-      </PopupWithForm>
-  )
+    <PopupWithForm
+      open={isOpen}
+      onClose={onClose}
+      title={"Edit profile photo"}
+      onSubmit={onSubmitAvatar}
+    >
+      <>
+        <input
+          type="url"
+          required
+          className="form form_input"
+          name="link"
+          ref={avatarRef}
+          placeholder="link"
+        />
+        <span className="form form_error form_error_index-name"></span>
+      </>
+    </PopupWithForm>
+  );
 }
--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,8 +1,8 @@
 import PopupWithForm from "./PopupWithForm";
 import React from "react";
-import  CurrentUserContext  from "../contexts/CurrentUserContext";
+import CurrentUserContext from "../contexts/CurrentUserContext";
 
-export default function EditProfilePopup ({isOpen, onClose, onUpdateUser }) {
+export default function EditProfilePopup({ isOpen, onClose, onUpdateUser }) {
   const [name, setName] = React.useState("");
   const [description, setDescription] = React.useState("");
 
@@ -11,53 +11,53 @@ export default function EditProfilePopup ({isOpen, onClose, onUpdateUser }) {
     setName(currenttUser.name);
     setDescription(currenttUser.about);
   }, [currenttUser]);
-  function onSubmitEditProfile(e){
-    e.preventDefault();
+
+  function onSubmitEditProfile() {
     onUpdateUser({
       name,
       about: description,
     });
   }
-  return (
 
-<PopupWithForm
-open={isOpen}
-onClose={onClose}
-title={"Edit profile"}
-onSubmit={onSubmitEditProfile}
->
-<>
-  <input
-    placeholder="Name"
-    minLength="2"
-    maxLength="40"
-    type="text"
-    id="input-name"
-    className="form form_input"
-    onChange={(e)=> setName(e.target.value)}
-    name="name"
-    defaultValue={currenttUser.name}
-    required
-  />
-  <span className="form form_error form_error_index-name"></span>
-  <label
-    htmlFor="input-name"
-    className="popup__label popup__label_edit"
-  ></label>
-  <input
-    placeholder="About"
-    required
-    minLength="2"
-    maxLength="200"
-    type="text"
-    id="input-about"
-    className="form form_input"
-    onChange={(e)=> setDescription(e.target.value)}
-    name="job"
-    defaultValue={currenttUser.about}
-  />
-  <span className="form form_error form_error_index-job"></span>
-</>
-</PopupWithForm>
-  )
+  return (
+    <PopupWithForm
+      open={isOpen}
+      onClose={onClose}
+      title={"Edit profile"}
+      onSubmit={onSubmitEditProfile}
+    >
+      <>
+        <input
+          placeholder="Name"
+          minLength="2"
+          maxLength="40"
+          type="text"
+          id="input-name"
+          className="form form_input"
+          onChange={(e) => setName(e.target.value)}
+          name="name"
+          defaultValue={currenttUser.name}
+          required
+        />
+        <span className="form form_error form_error_index-name"></span>
+        <label
+          htmlFor="input-name"
+          className="popup__label popup__label_edit"
+        ></label>
+        <input
+          placeholder="About"
+          required
+          minLength="2"
+          maxLength="200"
+          type="text"
+          id="input-about"
+          className="form form_input"
+          onChange={(e) => setDescription(e.target.value)}
+          name="job"
+          defaultValue={currenttUser.about}
+        />
+        <span className="form form_error form_error_index-job"></span>
+      </>
+    </PopupWithForm>
+  );
 }
