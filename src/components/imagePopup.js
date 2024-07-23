6b82b6101441a47dc25086a04a32ed23d14c6a27
Cesar 26 <cesarauge26@gmail.com>
2024-07-22 23:43:41 -0500

    version corrected 3

--- src/components/imagePopup.js
+++ src/components/imagePopup.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Popup from "./Popup";
+import CurrentUserContext from "../contexts/CurrentUserContext";
+
+
 
 export default function ImagePopup ({title, onClose, selectCard, open}){
-
+  const currenttUser = React.useContext(CurrentUserContext)
 
   return (
 
